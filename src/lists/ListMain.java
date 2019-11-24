40829d6433e67ba93e0a961c411b325aa46aac20
Robert Majerski <dupa@gmail.com>
2019-11-24 14:06:50 +0100

    Array list incremental

---
+++ src/lists/ListMain.java
@@ -0,0 +1,22 @@
+package lists;
+
+import java.util.Random;
+
+public class ListMain {
+    public static void main(String[] args) {
+        IList<Integer> list = new ArrayList1<>();
+        Random rand = new Random();
+        int numbersCount = 10;
+        for (int i = 0; i < numbersCount; i++) {
+            list.add(rand.nextInt(101));
+        }
+        ListUtilities.display(list);
+        list.add(11,2);
+        ListUtilities.display(list);
+        list.remove(3);
+        ListUtilities.display(list);
+        list.set(666,4);
+        ListUtilities.display(list);
+        System.out.println(list.get(4));
+    }
+}
